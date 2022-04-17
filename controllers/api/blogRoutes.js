c4e9f9b8c617b928c8e5e534a9e42cb93f61066d
seanlee1379@gmail.com <seanlee1379@gmial.com>
2022-04-17 13:14:36 -0700

    boiler plate

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const { Blog } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({});
+
+    res.status(200).json(blogData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {});
+    if (!blogData) {
+      res.status(404).json({ message: "No product found with that id!" });
+
+      return;
+    }
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", (req, res) => {
+  Blog.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: "No product found with this id" });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
